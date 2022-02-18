ccb47e7b89c98261f4af09ee3c2103a7bc195734
robinshrestha12 <robinshrestha.maple@gmail.com>
2022-02-18 14:01:46 -0500

    shortened the method

--- typewriter.js
+++ typewriter.js
@@ -1,13 +1,10 @@
 
-const sentence = "hello there from lighthouse labs";
+const sentence = "hello there from lighthouse labs\n";
 let time = 0;
 for (const char of sentence) { //iterating through characters
   time += 50; //setting counter
-  count = 0;
   setTimeout(() => {
     process.stdout.write(char);
   }, time);
 }
-setTimeout(() =>
-  process.stdout.write('\n') // fixing the new line at the end.
-  , time);
+
